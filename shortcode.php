0d4cfffa6239f5856141b28f5dc71e5b0972991e
Faisal Akandha <57830958+faisalakandha@users.noreply.github.com>
2023-06-14 19:01:08 +0600

    Update shortcode.php

--- shortcode.php
+++ shortcode.php
@@ -25,6 +25,49 @@ function SentenceSummary()
 			integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
 			crossorigin="anonymous"
 		></script>
+		<script>
+			const callApi = () => {
+    const text = textIp.value;
+    const len = text.split(" ").filter(function(n) {
+        return n !== "";
+    }).length;
+
+    if (len < 200) {
+        myFunction();
+        return;
+    }
+
+    const requestOptions = {
+        method: "POST",
+        body: JSON.stringify({ text: text }),
+        headers: {
+               'Content-Type': 'application/json',
+               'X-WP-Nonce': "<?php echo wp_create_nonce('wp_rest') ?>"
+        }
+    };
+
+    loader.style.display = "block";
+
+    fetch(siteUrl + '/wp-json/sentencesummary/v1/summary', requestOptions)
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.summary) {
+                summary.innerText = data.summary;
+                copyButton.style.display = "block";
+            } else {
+                summary.innerText = "Invalid";
+            }
+            setTimeout(() => {
+                window.scrollBy(0, 300);
+            }, 1000);
+        })
+        .catch((error) => console.log("error", error))
+        .finally(() => {
+            loader.style.display = "none";
+        });
+};
+
+		</script>
 	</head>
 	<body>
 		<div class="input-box">
