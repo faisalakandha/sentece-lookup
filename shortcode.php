24628768f8dcb8f5618cae8f5b37d4a3362c5d01
Faisal Akandha <57830958+faisalakandha@users.noreply.github.com>
2023-06-14 18:46:57 +0600

    Update shortcode.php

--- shortcode.php
+++ shortcode.php
@@ -19,7 +19,7 @@ function SentenceSummary()
 			integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
 			crossorigin="anonymous"
 		/>
-		<link rel="stylesheet" href="<?php echo plugin_dir_path(__FILE__) . 'style.css' ?>" />
+		<link rel="stylesheet" href="http://wp.docker.localhost:8000/wp-content/plugins/sentece-summary/style.css" />
 		<script
 			src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
 			integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
